224302ceeae95de783c32627d7d74dc93cdb3d9c
Artem-Ky <artoad505@mail.ru>
2024-08-12 02:38:26 +0300

    fix: api request auth

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -10,3 +10,12 @@ export const $api = axios.create({
             || '',
     },
 });
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY)
+            || sessionStorage.getItem(USER_LOCALSTORAGE_KEY)
+            || '';
+    }
+    return config;
+});
