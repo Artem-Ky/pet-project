1fc69b197d4bc9e0f6ddd4dd7b03065d67286074
Artem-Ky <artoad505@mail.ru>
2024-08-25 02:16:03 +0300

    feat: babel plugins

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,21 +3,24 @@ import { BuildOptions } from './types/config';
 import { BuildCSSLoader } from './loaders/BuildCSSLoader';
 import { BuildBabelLoader } from './loaders/BuildBabelLoader';
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+    const { isDev } = options;
+
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     };
 
-    const babelLoader = BuildBabelLoader(isDev);
+    const codeBabelLoader = BuildBabelLoader({ ...options, isTsx: false });
+    const tsxCodeBabelLoader = BuildBabelLoader({ ...options, isTsx: true });
 
     const cssLoader = BuildCSSLoader(isDev);
 
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    };
+    // const typescriptLoader = {
+    //     test: /\.tsx?$/,
+    //     use: 'ts-loader',
+    //     exclude: /node_modules/,
+    // };
 
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff2|woff)$/i,
@@ -38,8 +41,9 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
-        babelLoader,
-        typescriptLoader,
+        codeBabelLoader,
+        tsxCodeBabelLoader,
+        // typescriptLoader,
         cssLoader,
         mjsLoader,
     ];
