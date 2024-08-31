00be0849559514796244058e115c1606bb46f7d4
Artem-Ky <artoad505@mail.ru>
2024-08-31 19:23:17 +0300

    fix: navbar sticky

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,6 +2,7 @@ import { FC, memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { BugButton } from '@/app/providers/ErrorBoundary';
 import { Page } from '@/widgets/Page';
+import { VStack } from '@/shared/ui/Stack';
 
 const MainPage: FC = memo(() => {
     const { t } = useTranslation('mainPage');
@@ -10,6 +11,28 @@ const MainPage: FC = memo(() => {
         <Page>
             {t('Главная страница сайта', { ns: 'mainPage' })}
             <BugButton />
+            <VStack gap="32">
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+                <VStack>1</VStack>
+            </VStack>
         </Page>
     );
 });
