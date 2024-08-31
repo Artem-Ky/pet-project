47f048c74092c17c5b3443651200b7b58e99463a
Artem-Ky <artoad505@mail.ru>
2024-08-31 18:29:13 +0300

    feat: articleRating story

--- src/features/ArticleRating/ui/ArticleRating/ArticleRating.stories.tsx
+++ src/features/ArticleRating/ui/ArticleRating/ArticleRating.stories.tsx
@@ -3,12 +3,27 @@ import { Theme } from '@/shared/const/theme';
 import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 
 import ArticleRating from './ArticleRating';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 export default {
     title: 'features/ArticleRating',
     component: ArticleRating,
     argTypes: {
         backgroundColor: { control: 'color' },
+    },
+    parameters: {
+        mockData: [
+            {
+                url: `${__API__}/article-ratings?userId=1&articleId=1`,
+                method: 'GET',
+                status: 200,
+                response: [
+                    {
+                        rate: 4,
+                    },
+                ],
+            },
+        ],
     },
 } as Meta<typeof ArticleRating>;
 
@@ -17,8 +32,26 @@ const Template: StoryFn<typeof ArticleRating> = (args) => (
 );
 
 export const Light = Template.bind({});
-Light.args = {};
+Light.args = {
+    articleId: '1',
+};
+Light.decorators = [
+    StoreDecorator({
+        user: {
+            authData: { id: '1' },
+        },
+    }),
+];
 
 export const Dark = Template.bind({});
-Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.args = {
+    articleId: '1',
+};
+Dark.decorators = [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({
+        user: {
+            authData: { id: '1' },
+        },
+    }),
+];
